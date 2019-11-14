c1df8480bc8d5e3e0bfd81459fcebdf4f643e13b
KirilSoshnin <kiril@aurawebsolutions.com>
2019-11-13 22:00:11 -0500

    feat(portfolio): implement defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,4 +8,11 @@ class Portfolio < ApplicationRecord
 
   # Custom scope method 2
   scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= 'http://placehold.it/600/400'
+    self.thumb_image ||= 'http://placehold.it/300/150'
+  end
 end
