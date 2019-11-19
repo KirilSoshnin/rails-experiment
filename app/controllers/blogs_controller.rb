6491d51a812c6fd0eaf1723f7858333574a97b65
KirilSoshnin <kiril@aurawebsolutions.com>
2019-11-19 12:33:49 -0500

    feat: add navbar and default head content

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,11 +5,14 @@ class BlogsController < ApplicationController
   # GET /blogs.json
   def index
     @blogs = Blog.all
+    @page_title = "Blog | #{@page_title}"
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
-  def show; end
+  def show
+    @page_title = "#{@blog.title} | #{@page_title}"
+  end
 
   # GET /blogs/new
   def new
